71252fbf03dc8e4a58eecc7951bc2c79978e9fd6
railanabreu <railandeabreu@gmail.com>
2023-11-15 16:18:13 -0300

    [WIP] Ajuste nas rotas

--- backend/src/app/router/user.router.js
+++ backend/src/app/router/user.router.js
@@ -2,18 +2,18 @@ const router = require('express').Router();
 const registerController = require('../controller/register.controller');
 const resetSenha = require('../controller/recuperacaoSenha.controller');
 const userController = require('../controller/user.controller');
+const authToken = require('../middlewares/auth.middlewere');
 
 
 router.post('/register', registerController.createUser); 
-router.post('/recuperarSenha', resetSenha.recuperarSenha); 
 router.post('/forgotPasswordToken', resetSenha.recuperarSenha);
-router.post('/resetPassword', resetSenha.resetarSenha);
-router.post('/update', userController.updateUser);
-router.get('/findAll', userController.findAllUsers);
-router.get('/find/:id', userController.findUserById);
-router.get('/findUserById', userController.findUserById);
-router.post('/updateUser', userController.updateUser);
-router.post('/find', userController.findUser);
+router.post('/resetPassword',authToken, resetSenha.resetarSenha);
+router.post('/update',authToken, userController.updateUser);
+router.get('/findAll',authToken, userController.findAllUsers);
+router.get('/find/:id',authToken, userController.findUserById);
+router.get('/findUserById',authToken, userController.findUserById);
+router.post('/updateUser',authToken, userController.updateUser);
+router.post('/find',authToken, userController.findUser);
 
 
 module.exports = router;
