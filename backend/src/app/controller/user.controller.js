472ba7238b07337ca938bfd42b2cdc92c19efb3b
Guilherme Schlickmann <guilherme.cschlickmann@gmail.com>
2023-11-22 14:40:04 -0300

    fixed misspelling func on findUserById

--- backend/src/app/controller/user.controller.js
+++ backend/src/app/controller/user.controller.js
@@ -78,7 +78,7 @@ const findAllUsers = async (req, res) => {
 const findUserById = async (req, res) => {
     const { id } = req.params;
     try {
-        const user = await userServices.findUserServiceById(id);
+        const user = await userServices.findUserByIdService(id);
         return res.status(200).json({ user });
     } catch (error) {
         return res.status(400).json({ error: error.message });
