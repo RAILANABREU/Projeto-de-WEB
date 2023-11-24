a895462cffe2738aaa3f9510109ab6aa803b7025
JeniferOvejero <jenifersofiaovejero@gmail.com>
2023-11-24 03:37:30 -0300

    .

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Card } from "../components/common/Card";
 import Head from "../components/layout/Head";
 import Main from "../components/layout/Main";
 import { getAllEvents } from "../services/eventosSevices";
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { useEffect, useState } from "react";
 import Footer from "../components/layout/Footer";
 import ModalUser from "../components/common/ModalUser";
@@ -12,6 +12,7 @@ export default function Home() {
   const [eventos, setEventos] = useState([]);
   const [isDrawerOpen, setDrawerOpen] = useState(false);
   const { userId } = useParams();
+  const navigate  = useNavigate();
 
   useEffect(() => {
     async function fetchData() {
@@ -30,7 +31,9 @@ export default function Home() {
   const openDrawer = () => {
     setDrawerOpen(true);
   };
-
+  const eventoPage = (eventoId) =>{
+    navigate(`/evento/${userId}/${eventoId}`)
+  }
 
   return (
     <div className="page">
@@ -44,13 +47,16 @@ export default function Home() {
         <Link to={`/criarevento/${userId}`}><button>CRIAR EVENTO</button></Link>
         <Link to="/convites"><button style={{ background: "var(--secondary-color)" }}>CONVITES</button></Link>
         {eventos.length > 0 ? (
-          eventos.map((item, index) => (
-            <Card 
-            key={index} 
-            eventos={item} 
-            foto={item.imagem} 
-            titulo={item.titulo} 
-            />
+          eventos.map((item) => (
+            item.adm === userId ? (
+              <Card 
+                key={item.id} 
+                eventos={item} 
+                foto={item.imagem} 
+                titulo={item.titulo} 
+                //onClick={eventoPage(item.id)}
+              />
+            ) : null
           ))
         ) : (
           <div style={{ flex: 1}}>
