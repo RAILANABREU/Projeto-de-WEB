3bba4ad5b48dbc886e4dbb72381e10209d595947
Guilherme Schlickmann <guilherme.cschlickmann@gmail.com>
2023-11-28 07:25:26 -0300

    removi consoles.log(error)

--- backend/src/app/controller/convite.controller.js
+++ backend/src/app/controller/convite.controller.js
@@ -83,7 +83,7 @@ const enviarConvite = async (req, res) => {
                 user
                 });
             } catch (error) {
-                console.error(error);
+            
                 // Aqui você pode imprimir o erro para debug ou tratá-lo de outra forma
             }
     }
@@ -172,7 +172,6 @@ const enviarConvite = async (req, res) => {
                 userconvites: user.convites
             });
         } catch (error) {
-            console.error(error);
             res.status(500).send({
                 message: "Erro ao aceitar convite"
             });
@@ -196,7 +195,6 @@ const findConvidados = async (req, res) => {
             convidados: evento.convidados
         });
     } catch (error) {
-        console.error(error);
         res.status(500).send({
             message: "Erro ao listar convidados"
         });
@@ -243,7 +241,6 @@ const alterarConvidados = async (req, res) => {
             convidados: evento.convidados
         });
     } catch (error) {
-        console.error(error);
         res.status(500).send({
             message: "Erro ao atualizar convidado"
         });
@@ -282,7 +279,6 @@ const deletarConvidado = async (req, res) => {
         const convidado = await userService.findUserByIdService(idConvidado);
         const index = convidado.eventosConfirmados.indexOf(evento._id);
         if (index > -1){
-            console.log("entrou aqui")
             convidado.eventosConfirmados.splice(index, 1);
             await convidado.updateOne(convidado);
         }else{
